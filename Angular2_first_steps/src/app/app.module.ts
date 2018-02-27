c15ff17b9f8f0dac1ff88d665363242bd1db5889
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-02-27 22:18:06 +0100

    Create first components and assign simple routing. Remove unnecessery  method Use inside Startup.cs

--- Angular2_first_steps/src/app/app.module.ts
+++ Angular2_first_steps/src/app/app.module.ts
@@ -1,23 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+
+import { Router } from '@angular/router';
+
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { DirectorListComponent } from './director-list/director-list.component';
+import { DirectorDetailComponent } from './director-detail/director-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    MovieListComponent,
+    MovieDetailComponent,
+    DirectorListComponent,
+    DirectorDetailComponent,
+    NotFoundComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
---
+++ Angular2_first_steps/src/app/director-detail/director-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-detail',
+  templateUrl: './director-detail.component.html',
+  styleUrls: ['./director-detail.component.css']
+})
+export class DirectorDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
