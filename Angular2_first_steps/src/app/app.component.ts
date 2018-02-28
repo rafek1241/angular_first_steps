5ac3d2775a2d1a5264223fe6e4c62dc53a5b3e20
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-02-28 20:38:12 +0100

    Revert "Create first components and assign simple routing. Remove unnecessery  method Use inside Startup.cs"
    This reverts commit c15ff17b9f8f0dac1ff88d665363242bd1db5889.

--- Angular2_first_steps/src/app/app.component.ts
+++ Angular2_first_steps/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-root',
-  template: `<router-outlet></router-outlet>`
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    constructor(private _httpService: Http) { }
+
+    apiValues: string[] = [];
+
+    ngOnInit() {
+        this._httpService.get('api/values').subscribe(p => {
+            this.apiValues = p.json() as string[];
+        });
+    }
 }
--- Angular2_first_steps/src/app/app.module.ts
+++ Angular2_first_steps/src/app/app.module.ts
@@ -1,37 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { Router } from '@angular/router';
 
-import { NotFoundComponent } from './not-found/not-found.component';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { MovieDetailComponent } from './movie-detail/movie-detail.component';
-import { DirectorListComponent } from './director-list/director-list.component';
-import { DirectorDetailComponent } from './director-detail/director-detail.component';
-import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    MovieListComponent,
-    MovieDetailComponent,
-    DirectorListComponent,
-    DirectorDetailComponent,
-    NotFoundComponent
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {
-  // Diagnostic only: inspect router configuration
-  constructor(router: Router) {
-    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
-  }
-}
+export class AppModule { }
