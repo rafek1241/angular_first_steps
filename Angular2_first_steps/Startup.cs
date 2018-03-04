2194dcf10bd30fd8fc0ced37c31e3c03a470f6d9
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-03-04 19:26:30 +0100

    Implementation seeder class to seed database on startup (only if database is empty of course). In EF core seed method doesn't exists.

--- Angular2_first_steps/Startup.cs
+++ Angular2_first_steps/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using angular.dao.Migrations;
 using angular.dao.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,30 +9,36 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Angular2_first_steps
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc(); var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
-          services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
+      Configuration = configuration;
     }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
+    public IConfiguration Configuration { get; }
 
-            app.UseMvcWithDefaultRoute();
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles(); 
-        }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc(); var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+      services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection")));
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+      {
+        var context = serviceScope.ServiceProvider.GetService<Context>();
+        context.Database.Migrate();
+        context.Seed();
+      }
+
+      app.UseMvcWithDefaultRoute();
+
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+    }
+  }
 }
---
+++ angular.dao/Migrations/Seeder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using angular.dao.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace angular.dao.Migrations
+{
+    public static class Seeder
+    {
+        public static void Seed(this Context ctx)
+        {
+            if (ctx.Database.GetPendingMigrations().Any()) return;
+
+            if (!ctx.Users.Any())
+            {
+                ctx.Users.Add(new User()
+                {
+                    BirthDay = new DateTime(1995, 10, 13),
+                    Email = "admin@admin.pl",
+                    Login = "admin",
+                    Name = "Administrator",
+                    Password = "admin",
+                    Ratings = new List<Rating>()
+                });
+            }
+
+            ctx.SaveChanges();
+        }
+    }
+}
