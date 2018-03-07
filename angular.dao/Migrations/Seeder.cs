243d268dcfb67cb0e7eb5e5a5b025d5c8ddcf219
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-03-07 19:05:08 +0100

    update database seed.

--- angular.dao/Migrations/Seeder.cs
+++ angular.dao/Migrations/Seeder.cs
@@ -25,6 +25,53 @@ namespace angular.dao.Migrations
                 });
             }
 
+            if (!ctx.Actors.Any())
+            {
+                ctx.Actors.Add(new Actor()
+                {
+                    FirstName = "Jimmy",
+                    Surname = "Carrey",
+                    BirthDate = new DateTime(1995,10,1)
+                });
+            }
+            
+            if (!ctx.Movies.Any())
+            {
+                ctx.Movies.Add(new Movie()
+                {
+                    Director = null,
+                    Ratings = null,
+                    ActorMovies = new List<ActorMovie>(),
+                    PublishDate = DateTime.Now,
+                    Title = "Godfather"
+                });
+                ctx.Movies.Add(new Movie()
+                {
+                    Director = null,
+                    Ratings = null,
+                    ActorMovies = new List<ActorMovie>(),
+                    PublishDate = DateTime.Now,
+                    Title = "Godfather II"
+                });
+                ctx.Movies.Add(new Movie()
+                {
+                    Director = null,
+                    Ratings = null,
+                    ActorMovies = new List<ActorMovie>(),
+                    PublishDate = DateTime.Now,
+                    Title = "Star wars"
+                });
+                ctx.Movies.Add(new Movie()
+                {
+                    Director = null,
+                    Ratings = null,
+                    ActorMovies = new List<ActorMovie>(),
+                    PublishDate = DateTime.Now,
+                    Title = "Scarface"
+                });
+            }
+
+
             ctx.SaveChanges();
         }
     }
