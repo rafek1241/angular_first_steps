360f25e119092bb5efb3d6db308408af2dc02a13
Robert Szachnowski <rszachnowski@wi.ps.pl>
2018-03-19 19:53:09 +0100

    Added  wrapper to controllerbase class for service implementation (to use IActionResult methods)

---
+++ angular.services/Utils/ControllerBaseWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace angular.services.Utils
+{
+    public abstract class ControllerBaseWrapper : ControllerBase
+    {
+        [NonAction]
+        public virtual CreatedResult Created(object model)
+        {
+            return (CreatedResult)new ObjectResult(model){StatusCode = 201};
+        }
+    }
+}
